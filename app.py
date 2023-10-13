4f9678e6e2addb05eba978a0fda94586d54a01f0
Alonso Silva Allende <alonso.silva@gmail.com>
2023-10-13 16:42:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def get_shap_values():
     prompt = """
     The 2008 Summer Olympics torch relay was run from March 24 until August 8, 2008, prior to the 2008 Summer Olympics, with the theme of "one world, one dream". Plans for the relay were announced on April 26, 2007, in Beijing, China. The relay, also called by the organizers as the "Journey of Harmony", lasted 129 days and carried the torch 137,000 km (85,000 mi) – the longest distance of any Olympic torch relay since the tradition was started ahead of the 1936 Summer Olympics.
 
-    Q: What was the theme
+    Q: What was the theme?
     A:
     """.strip()
     
@@ -24,16 +24,14 @@ def get_shap_values():
     # Sample instead of returning the most likely token
     model.config.do_sample=False
     # Set maximum length
-    model.config.max_new_tokens = 10
+    model.config.max_new_tokens = 13
     # Generate text with stop_token set to "."
     output = model.generate(input_ids)
-    st.write("Hello 1")
-    output_text = tokenizer.decode(output[0][-10:], skip_special_tokens=True)
+    output_text = tokenizer.decode(output[0][-13:], skip_special_tokens=True)
     
     model.config.is_decoder = True
     explainer = shap.Explainer(model, tokenizer)
     shap_values = explainer([prompt])
-    st.write("Hello 2")
     return shap_values
 
 shap_values = get_shap_values()
